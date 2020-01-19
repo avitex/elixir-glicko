641c661b4559cf0861b9076774f7533df7dcc7ea
Mikael Muszynski <linduxed@gmail.com>
2020-01-19 14:44:16 +0100

    Run `mix format`
    Add formatter configuration and run `mix format`.
    
    Additionally, manually replace leading tabs in documentation examples
    with four spaces.

--- test/glicko_test.exs
+++ test/glicko_test.exs
@@ -1,68 +1,77 @@
 defmodule GlickoTest do
-	use ExUnit.Case
+  use ExUnit.Case
 
-	alias Glicko.{
-		Player,
-		Result,
-	}
+  alias Glicko.{
+    Player,
+    Result
+  }
 
-	doctest Glicko
+  doctest Glicko
 
-	@player [rating: 1500, rating_deviation: 200] |> Player.new_v1 |> Player.to_v2
+  @player [rating: 1500, rating_deviation: 200] |> Player.new_v1() |> Player.to_v2()
 
-	@results [
-		Result.new(Player.new_v1([rating: 1400, rating_deviation: 30]), :win),
-		Result.new(Player.new_v1([rating: 1550, rating_deviation: 100]), :loss),
-		Result.new(Player.new_v1([rating: 1700, rating_deviation: 300]), :loss),
-	]
+  @results [
+    Result.new(Player.new_v1(rating: 1400, rating_deviation: 30), :win),
+    Result.new(Player.new_v1(rating: 1550, rating_deviation: 100), :loss),
+    Result.new(Player.new_v1(rating: 1700, rating_deviation: 300), :loss)
+  ]
 
-	@valid_player_rating_after_results 1464.06 |> Player.scale_rating_to(:v2)
-	@valid_player_rating_deviation_after_results 151.52 |> Player.scale_rating_deviation_to(:v2)
-	@valid_player_volatility_after_results 0.05999
+  @valid_player_rating_after_results 1464.06 |> Player.scale_rating_to(:v2)
+  @valid_player_rating_deviation_after_results 151.52 |> Player.scale_rating_deviation_to(:v2)
+  @valid_player_volatility_after_results 0.05999
 
-	@valid_player_rating_deviation_after_no_results 200.2714 |> Player.scale_rating_deviation_to(:v2)
+  @valid_player_rating_deviation_after_no_results 200.2714
+                                                  |> Player.scale_rating_deviation_to(:v2)
 
-	describe "new rating" do
-		test "with results" do
-			player = Glicko.new_rating(@player, @results, [system_constant: 0.5])
-	
-			assert_in_delta Player.rating(player), @valid_player_rating_after_results, 1.0e-4
-			assert_in_delta Player.rating_deviation(player), @valid_player_rating_deviation_after_results, 1.0e-4
-			assert_in_delta Player.volatility(player), @valid_player_volatility_after_results, 1.0e-5
-		end
-	
-		test "no results" do
-			player = Glicko.new_rating(@player, [])
-	
-			assert_in_delta Player.rating_deviation(player), @valid_player_rating_deviation_after_no_results, 1.0e-4
-		end
-	end
+  describe "new rating" do
+    test "with results" do
+      player = Glicko.new_rating(@player, @results, system_constant: 0.5)
 
-	describe "win probability" do
-		test "with same ratings" do
-			assert Glicko.win_probability(Player.new_v1, Player.new_v1) == 0.5
-		end
-	
-		test "with better opponent" do
-			assert Glicko.win_probability(Player.new_v1([rating: 1500]), Player.new_v1([rating: 1600])) < 0.5
-		end
-	
-		test "with better player" do
-			assert Glicko.win_probability(Player.new_v1([rating: 1600]), Player.new_v1([rating: 1500])) > 0.5
-		end
-	end
-	
-	describe "draw probability" do
-		test "with same ratings" do
-			assert Glicko.draw_probability(Player.new_v1, Player.new_v1) == 1
-		end
-	
-		test "with better opponent" do
-			assert Glicko.draw_probability(Player.new_v1([rating: 1500]), Player.new_v1([rating: 1600])) < 1
-		end
-	
-		test "with better player" do
-			assert Glicko.draw_probability(Player.new_v1([rating: 1600]), Player.new_v1([rating: 1500])) < 1
-		end
-	end
+      assert_in_delta Player.rating(player), @valid_player_rating_after_results, 1.0e-4
+
+      assert_in_delta Player.rating_deviation(player),
+                      @valid_player_rating_deviation_after_results,
+                      1.0e-4
+
+      assert_in_delta Player.volatility(player), @valid_player_volatility_after_results, 1.0e-5
+    end
+
+    test "no results" do
+      player = Glicko.new_rating(@player, [])
+
+      assert_in_delta Player.rating_deviation(player),
+                      @valid_player_rating_deviation_after_no_results,
+                      1.0e-4
+    end
+  end
+
+  describe "win probability" do
+    test "with same ratings" do
+      assert Glicko.win_probability(Player.new_v1(), Player.new_v1()) == 0.5
+    end
+
+    test "with better opponent" do
+      assert Glicko.win_probability(Player.new_v1(rating: 1500), Player.new_v1(rating: 1600)) <
+               0.5
+    end
+
+    test "with better player" do
+      assert Glicko.win_probability(Player.new_v1(rating: 1600), Player.new_v1(rating: 1500)) >
+               0.5
+    end
+  end
+
+  describe "draw probability" do
+    test "with same ratings" do
+      assert Glicko.draw_probability(Player.new_v1(), Player.new_v1()) == 1
+    end
+
+    test "with better opponent" do
+      assert Glicko.draw_probability(Player.new_v1(rating: 1500), Player.new_v1(rating: 1600)) < 1
+    end
+
+    test "with better player" do
+      assert Glicko.draw_probability(Player.new_v1(rating: 1600), Player.new_v1(rating: 1500)) < 1
+    end
+  end
 end
--- test/player_test.exs
+++ test/player_test.exs
@@ -1,60 +1,62 @@
 defmodule Glicko.PlayerTest do
-	use ExUnit.Case
+  use ExUnit.Case
 
-	alias Glicko.Player
+  alias Glicko.Player
 
-	doctest Player
+  doctest Player
 
-	@valid_v1_base {1.0, 2.0}
-	@valid_v2_base {1.0, 2.0, 3.0}
+  @valid_v1_base {1.0, 2.0}
+  @valid_v2_base {1.0, 2.0, 3.0}
 
-	test "create v1" do
-		assert @valid_v1_base == Player.new_v1([rating: 1.0, rating_deviation: 2.0])
-	end
+  test "create v1" do
+    assert @valid_v1_base == Player.new_v1(rating: 1.0, rating_deviation: 2.0)
+  end
 
-	test "create v2" do
-		assert @valid_v2_base == Player.new_v2([rating: 1.0, rating_deviation: 2.0, volatility: 3.0])
-	end
+  test "create v2" do
+    assert @valid_v2_base == Player.new_v2(rating: 1.0, rating_deviation: 2.0, volatility: 3.0)
+  end
 
-	test "convert player v1 -> v2" do
-		assert {Player.scale_rating_to(1.0, :v2), Player.scale_rating_deviation_to(2.0, :v2), 3.0} == Player.to_v2(@valid_v1_base, 3.0)
-	end
+  test "convert player v1 -> v2" do
+    assert {Player.scale_rating_to(1.0, :v2), Player.scale_rating_deviation_to(2.0, :v2), 3.0} ==
+             Player.to_v2(@valid_v1_base, 3.0)
+  end
 
-	test "convert player v2 -> v1" do
-		assert {Player.scale_rating_to(1.0, :v1), Player.scale_rating_deviation_to(2.0, :v1)} == Player.to_v1(@valid_v2_base)
-	end
+  test "convert player v2 -> v1" do
+    assert {Player.scale_rating_to(1.0, :v1), Player.scale_rating_deviation_to(2.0, :v1)} ==
+             Player.to_v1(@valid_v2_base)
+  end
 
-	test "convert player v1 -> v1" do
-		assert @valid_v1_base == Player.to_v1(@valid_v1_base)
-	end
+  test "convert player v1 -> v1" do
+    assert @valid_v1_base == Player.to_v1(@valid_v1_base)
+  end
 
-	test "convert player v2 -> v2" do
-		assert @valid_v2_base == Player.to_v2(@valid_v2_base)
-	end
+  test "convert player v2 -> v2" do
+    assert @valid_v2_base == Player.to_v2(@valid_v2_base)
+  end
 
-	test "scale rating v1 -> v2" do
-		assert_in_delta Player.scale_rating_to(1673.7178, :v2), 1.0, 0.1
-	end
+  test "scale rating v1 -> v2" do
+    assert_in_delta Player.scale_rating_to(1673.7178, :v2), 1.0, 0.1
+  end
 
-	test "scale rating v2 -> v1" do
-		assert_in_delta Player.scale_rating_to(1.0, :v1), 1673.7178, 0.1
-	end
+  test "scale rating v2 -> v1" do
+    assert_in_delta Player.scale_rating_to(1.0, :v1), 1673.7178, 0.1
+  end
 
-	test "scale rating deviation v1 -> v2" do
-		assert_in_delta Player.scale_rating_deviation_to(173.7178, :v2), 1.0, 0.1
-	end
+  test "scale rating deviation v1 -> v2" do
+    assert_in_delta Player.scale_rating_deviation_to(173.7178, :v2), 1.0, 0.1
+  end
 
-	test "scale rating deviation v2 -> v1" do
-		assert_in_delta Player.scale_rating_deviation_to(1.0, :v1), 173.7178, 0.1
-	end
+  test "scale rating deviation v2 -> v1" do
+    assert_in_delta Player.scale_rating_deviation_to(1.0, :v1), 173.7178, 0.1
+  end
 
-	test "rating interval" do
-		assert {rating_low, rating_high} =
-			[rating: 1850, rating_deviation: 50]
-			|> Player.new_v2
-			|> Player.rating_interval
+  test "rating interval" do
+    assert {rating_low, rating_high} =
+             [rating: 1850, rating_deviation: 50]
+             |> Player.new_v2()
+             |> Player.rating_interval()
 
-		assert_in_delta rating_low, 1750, 0.1
-		assert_in_delta rating_high, 1950, 0.1
-	end
+    assert_in_delta rating_low, 1750, 0.1
+    assert_in_delta rating_high, 1950, 0.1
+  end
 end
