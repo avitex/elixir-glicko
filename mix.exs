ca76ae6f9a144a9e641971f8e08403ebfec0822f
Mikael Muszynski <linduxed@gmail.com>
2020-01-27 05:40:17 +0100

    Update inch_ex hex (#5)
    Update `inch_ex` hex and add to the `dev` environment

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Glicko.Mixfile do
 
   defp deps do
     [
-      {:inch_ex, "~> 0.5", only: :docs},
+      {:inch_ex, "~> 2.0", only: [:dev, :docs]},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false},
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
     ]
